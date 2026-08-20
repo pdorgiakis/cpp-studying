4f58fc4722c680a0b73771747ae0d7f3d7cb1921
Panagiotis Dorgiakis <pdorgiakis@skroutz.gr>
2026-08-20 14:27:55 +0300

    Add config Struct to carry global info

--- gamedev/DapperDasher/main.cpp
+++ gamedev/DapperDasher/main.cpp
@@ -1,5 +1,15 @@
 #include "raylib.h"
 
+struct Config {
+  static const int window_width{800}, window_height{450};
+  static const int h_height{50}, h_width{50};
+  constexpr static const float h_init_pos_x{window_width / 2},
+      h_init_pos_y{window_height - 50};
+  static const int jump_height{200};
+  int velocity{0};
+};
+Config config;
+
 void DrawWindow(int width, int height) {
   SetTargetFPS(60);
   InitWindow(width, height, "Dapper Dasher");
