a7f99739cf0f96380332c1da199ac810f225ad75
Panagiotis Dorgiakis <pdorgiakis@skroutz.gr>
2026-08-20 12:40:33 +0300

    Add build command
    Run it with ```bash build.bash ./main.cpp```

---
+++ build.bash
@@ -0,0 +1,6 @@
+bear -- gcc $1 -o main \
+  -I$(brew --prefix raylib)/include -L$(brew --prefix raylib)/lib -lraylib \
+  -framework OpenGL \
+  -framework IOKit \
+  -framework Cocoa \
+  -framework CoreVideo
