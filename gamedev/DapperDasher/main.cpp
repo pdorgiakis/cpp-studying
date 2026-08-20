cdc9819391132df5ced100b2b1f2692c78b47af9
Panagiotis Dorgiakis <pdorgiakis@skroutz.gr>
2026-08-20 14:28:24 +0300

    Draw hero and implement jump logic

--- gamedev/DapperDasher/main.cpp
+++ gamedev/DapperDasher/main.cpp
@@ -10,25 +10,56 @@ struct Config {
 };
 Config config;
 
-void DrawWindow(int width, int height) {
+void DrawWindow() {
+  InitWindow(config.window_width, config.window_height, "Dapper Dasher");
   SetTargetFPS(60);
-  InitWindow(width, height, "Dapper Dasher");
 }
 
-void GameLoop(int width, int height) {
+bool IsTouchingTheGround(Rectangle *hero) {
+  if (hero->y >= Config::window_height - hero->height) {
+    return true;
+  }
+
+  return false;
+}
+
+void ControlHero(Rectangle *hero) {
+  hero->y -= config.velocity;
+
+  if (hero->y < Config::window_height - Config::jump_height) {
+    config.velocity = -config.velocity;
+  }
+
+  if (hero->y > config.window_height - 50.0f) {
+    config.velocity = 0.0f;
+    hero->y = config.window_height - 50.0f;
+  }
+  if (IsKeyDown(KEY_SPACE) && IsTouchingTheGround(hero)) {
+    config.velocity = 10.0f;
+  }
+}
+
+void GameLoop() {
+  Rectangle hero =
+      (Rectangle){Config::h_init_pos_x, Config::h_init_pos_y, 50, 50};
+
   while (!WindowShouldClose()) {
+    // Start Drawing
     BeginDrawing();
     ClearBackground(RAYWHITE);
+
+    ControlHero(&hero);
+
+    DrawRectangleRec(hero, RED);
+    // Stop Drawing
     EndDrawing();
   }
   CloseWindow();
 }
 
 int main(void) {
-  const int window_width{800}, window_height{450};
-
-  DrawWindow(window_width, window_height);
-  GameLoop(window_width, window_height);
+  DrawWindow();
+  GameLoop();
   CloseWindow();
   return 0;
 }
