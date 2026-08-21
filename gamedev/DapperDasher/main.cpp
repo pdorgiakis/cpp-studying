6bc302a21c401a92c81a47079d864c46d484b9b9
Panagiotis Dorgiakis <pdorgiakis@skroutz.gr>
2026-08-21 21:00:18 +0300

    Refactor structs to use common AnimData struct

--- gamedev/DapperDasher/main.cpp
+++ gamedev/DapperDasher/main.cpp
@@ -10,64 +10,79 @@ struct Config {
   static const int gravity{1000};
   // pixels per second
 
-  static const int ground{window_height - 50};
+  static const int scarfy_jump_velocity{-500};
 };
 
-struct Scarfy {
-  Texture2D scarfy_texture = LoadTexture("textures/scarfy.png");
+struct AnimData {
+  Texture2D texture;
+  Rectangle rec;
+  Vector2 pos;
+  Vector2 initial_frame_pos = {0, 0};
   int frame{0};
-  const int max_frames{6};
-  Rectangle scarfy_rec = {0, 0, float(scarfy_texture.width / max_frames),
-                          float(scarfy_texture.height)};
-  Vector2 scarfy_pos = {float(Config::window_width / 2 - scarfy_rec.width / 2),
-                        float(Config::ground - scarfy_rec.height)};
+  int max_frames;
   int velocity{0};
-  constexpr static const float jump_velocity{-500.0f};
+  float update_time;
+  float running_time{0.0f};
 
   void next_frame() {
-    scarfy_rec.x = frame * scarfy_rec.width;
+    if (update_time > running_time) {
+      return;
+    }
+    rec.x = initial_frame_pos.x + frame * rec.width;
     frame++;
     if (frame >= max_frames) {
       frame = 0;
     }
+    running_time = 0.0f;
   }
+  void draw() { DrawTextureRec(texture, rec, pos, WHITE); }
 };
 
-struct Enemy {
-  Texture2D enemy_texture = LoadTexture("textures/Creature_16x16.png");
-  int frame{0};
-  const int max_frames{4};
-  const int initial_frame_pos{64};
-  Rectangle enemy_rec = {64, 128, 16.0f, 16.0f};
-  Rectangle enemy_resize_rec = {Config::window_width, Config::ground,
-                                enemy_rec.width * 5.0f,
-                                enemy_rec.height * 5.0f};
-  Vector2 enemy_pos = {enemy_resize_rec.width, enemy_resize_rec.height};
-  int velocity{-300};
+AnimData InitializeScarfy() {
+  AnimData data;
+  data.texture = LoadTexture("textures/scarfy.png");
+  data.max_frames = 6;
+  data.rec.x = data.initial_frame_pos.x;
+  data.rec.y = data.initial_frame_pos.y;
+  data.rec.width = data.texture.width / data.max_frames;
+  data.rec.height = data.texture.height;
+  data.pos.x = Config::window_width / 2 - data.rec.width / 2;
+  data.pos.y = Config::window_height - data.rec.height;
+  data.update_time = 1.0f / 12.0f;
 
-  void next_frame() {
-    enemy_rec.x = initial_frame_pos + frame * enemy_rec.width;
-    frame++;
-    if (frame >= max_frames) {
-      frame = 0;
-    }
-  }
-};
+  return data;
+}
+
+AnimData InitializeEnemy() {
+  AnimData data;
+  data.texture = LoadTexture("textures/12_nebula_spritesheet.png");
+  data.max_frames = 8;
+  data.rec.x = data.initial_frame_pos.x;
+  data.rec.y = data.initial_frame_pos.y;
+  data.rec.width = data.texture.width / data.max_frames;
+  data.rec.height = data.texture.height / 8;
+  data.pos.x = Config::window_width;
+  data.pos.y = Config::window_height - data.rec.height;
+  data.update_time = 1.0f / 30.0f;
+  data.velocity = -300;
+
+  return data;
+}
 
 void DrawWindow() {
   InitWindow(Config::window_width, Config::window_height, "Dapper Dasher");
   SetTargetFPS(60);
 }
 
-bool IsTouchingTheGround(Scarfy *hero) {
-  if (hero->scarfy_pos.y >= Config::ground - hero->scarfy_rec.height) {
+bool IsTouchingTheGround(AnimData *hero) {
+  if (hero->pos.y >= Config::window_height - hero->rec.height) {
     return true;
   }
 
   return false;
 }
 
-void ControlHero(Scarfy *hero, float delta_time) {
+void ControlHero(AnimData *hero, float delta_time) {
 
   if (IsTouchingTheGround(hero)) {
     hero->velocity = 0.0f;
@@ -76,26 +91,22 @@ void ControlHero(Scarfy *hero, float delta_time) {
   }
 
   if (IsKeyDown(KEY_SPACE) && IsTouchingTheGround(hero)) {
-    hero->velocity = hero->jump_velocity;
+    hero->velocity = Config::scarfy_jump_velocity;
   }
 
-  hero->scarfy_pos.y += hero->velocity * delta_time;
+  hero->pos.y += hero->velocity * delta_time;
 }
 
-void MoveEnemy(Enemy *enemy, float delta_time) {
-  if (enemy->enemy_resize_rec.x < 0) {
-    enemy->enemy_resize_rec.x = Config::window_width;
+void MoveEnemy(AnimData *enemy, float delta_time) {
+  if (enemy->pos.x < 0) {
+    enemy->pos.x = Config::window_width;
   }
-  enemy->enemy_resize_rec.x += enemy->velocity * delta_time;
+  enemy->pos.x += enemy->velocity * delta_time;
 }
 
 void GameLoop() {
-  Scarfy scarfy;
-  Enemy enemy;
-  int scarfy_frame = 1;
-
-  float update_time = 1.0f / 5.0f;
-  float running_time{0.0f};
+  AnimData scarfy = InitializeScarfy();
+  AnimData enemy = InitializeEnemy();
 
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
@@ -108,32 +119,23 @@ void GameLoop() {
     MoveEnemy(&enemy, dt);
 
     // Draw Textures
-    DrawTextureRec(scarfy.scarfy_texture, scarfy.scarfy_rec, scarfy.scarfy_pos,
-                   WHITE);
-    DrawTexturePro(enemy.enemy_texture, enemy.enemy_rec, enemy.enemy_resize_rec,
-                   enemy.enemy_pos, 0.0f, WHITE);
+    scarfy.draw();
+    enemy.draw();
 
-    if (CheckCollisionRecs(scarfy.scarfy_rec, enemy.enemy_rec)) {
-      DrawText("You hit the axe!", 400, 200, 20, RED);
-    }
-
-    running_time += dt;
+    scarfy.running_time += dt;
+    enemy.running_time += dt;
 
     // Animate
-    if (running_time >= update_time) {
-      if (IsTouchingTheGround(&scarfy)) {
-        scarfy.next_frame();
-      }
-      enemy.next_frame();
-
-      running_time = 0.0f;
+    if (IsTouchingTheGround(&scarfy)) {
+      scarfy.next_frame();
     }
+    enemy.next_frame();
 
     // Stop Drawing
     EndDrawing();
   }
   CloseWindow();
-  UnloadTexture(scarfy.scarfy_texture);
+  UnloadTexture(scarfy.texture);
 }
 
 int main(void) {
