befe07be2f7953105e6ebbf3c26f225a8932e475
Panagiotis Dorgiakis <pdorgiakis@skroutz.gr>
2026-08-23 21:12:50 +0300

    Add config values to control map speed and scale

--- gamedev/ClassyClash/main.cpp
+++ gamedev/ClassyClash/main.cpp
@@ -4,6 +4,8 @@
 
 struct Config {
   static const int window_width{384}, window_height{384};
+  constexpr static const float movement_speed{5.0f};
+  constexpr static const float map_scale{4.0f};
 };
 
 void SetupWindow() {
@@ -14,13 +16,13 @@ void SetupWindow() {
 Vector2 GetMapDirection() {
   Vector2 direction{};
   if (IsKeyDown(KEY_RIGHT))
-    direction.x -= 10;
+    direction.x -= 1;
   if (IsKeyDown(KEY_LEFT))
-    direction.x += 10;
+    direction.x += 1;
   if (IsKeyDown(KEY_UP))
-    direction.y += 10;
+    direction.y += 1;
   if (IsKeyDown(KEY_DOWN))
-    direction.y -= 10;
+    direction.y -= 1;
 
   return direction;
 }
@@ -29,20 +31,24 @@ void GameLoop() {
   // Map Data
   Texture2D map_texture = LoadTexture("./textures/map.png");
   Vector2 map_position = {0, 0};
-  Vector2 min_map_limit = {Config::window_width - map_texture.width * 4.0f,
-                           Config::window_height - map_texture.height * 4.0f};
+  Vector2 min_map_limit = {
+      Config::window_width - map_texture.width * Config::map_scale,
+      Config::window_height - map_texture.height * Config::map_scale};
   Vector2 max_map_limit = {0, 0};
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(WHITE);
     // Logic Start
     float dt = GetFrameTime();
+
     Vector2 direction = GetMapDirection();
     if (Vector2Length(direction) != 0) {
-      map_position = Vector2Add(map_position, direction);
+      map_position = Vector2Add(
+          map_position, Vector2Scale(direction, Config::movement_speed));
+
       map_position = Vector2Clamp(map_position, min_map_limit, max_map_limit);
     }
-    DrawTextureEx(map_texture, map_position, 0.0f, 4.0f, WHITE);
+    DrawTextureEx(map_texture, map_position, 0.0f, Config::map_scale, WHITE);
     // Logic End
     EndDrawing();
   }
