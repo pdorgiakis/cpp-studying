27087d04d138da14c52613ce17a1f8f6460a53f6
Panagiotis Dorgiakis <pdorgiakis@skroutz.gr>
2026-09-01 20:58:44 +0300

    Check wasd for moving the character

--- gamedev/ClassyClash/character.cpp
+++ gamedev/ClassyClash/character.cpp
@@ -21,13 +21,13 @@ Character::Character() {
 void Character::Tick(float delta_time) {
 
   Vector2 direction{};
-  if (IsKeyDown(KEY_RIGHT))
+  if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
     direction.x -= 1;
-  if (IsKeyDown(KEY_LEFT))
+  if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
     direction.x += 1;
-  if (IsKeyDown(KEY_UP))
+  if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
     direction.y += 1;
-  if (IsKeyDown(KEY_DOWN))
+  if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
     direction.y -= 1;
 
   if (Vector2Length(direction) != 0) {
